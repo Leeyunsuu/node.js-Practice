d3716ff6651cbfb15762d70fb7983abc823dda3e
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-03-22 17:40:42 +0900

    중복코드 함수화

--- main.js
+++ main.js
@@ -3,30 +3,8 @@ var fs = require('fs');
 var url = require('url');
 const testFolder = './data';
 
-var app = http.createServer((request, response) => {
-	var _url = request.url;
-	var queryData = url.parse(_url, true).query;
-	var pathname = url.parse(_url, true).pathname;
-	var title = queryData.id; //http://localhost:3000/?id=ㅁㅁㅁ,	만약 id가 아닌 name이라면 name으로 입력
-	// console.log(url.parse(_url, true));
-	// console.log(request.url);
-	if (pathname === '/') {
-		if (queryData.id === undefined) {
-			title = 'Welcome';
-			let description = 'Hello, Node.js ^^'; //서버시작 후, description에 본문 생성.
-			fs.readdir('./data', (err, files) => {
-				let i = 0;
-				let lists = '<ul>';
-				// console.log(files);
-				while (i < 3) {
-					let filelist = files[i];
-					// console.log(filelist);
-					lists = lists + `<li><a href="/?id=${filelist}">${filelist}</a></li>`; //반복 할 때 마다 전에 할당한 <li>를 재할당
-					i = i + 1;
-				}
-				lists = lists + '</ul>';
-				var template = `
-			<!doctype html>
+function templateHTML(title, lists, body) {
+	return `<!doctype html>
 		<html>
 		<head>
 			<title>WEB1 - ${title}</title>
@@ -35,45 +13,47 @@ var app = http.createServer((request, response) => {
 		<body>
 			<h1><a href="/?id=WEB">WEB</a></h1>
 				${lists}
-			<h2>${title}</h2>
-			<p>${description}
-			</p>
-			</body>
-			</html>
-			`;
+				${body}
+		</body>
+		</html>
+	`;
+}
+
+function FileList(files) {
+	let i = 0;
+	var lists = '<ul>'; // console.log(files);
+	while (i < 3) {
+		let filelist = files[i]; // console.log(filelist);
+		lists = lists + `<li><a href="/?id=${filelist}">${filelist}</a></li>`; //반복 할 때 마다 전에 할당한 <li>를 재할당
+		i = i + 1;
+	}
+	lists = lists + '</ul>';
+	return lists;
+}
+
+var app = http.createServer((request, response) => {
+	var _url = request.url;
+	var queryData = url.parse(_url, true).query;
+	var pathname = url.parse(_url, true).pathname;
+	// console.log(url.parse(_url, true));
+	// console.log(request.url);
+	if (pathname === '/') {
+		if (queryData.id === undefined) {
+			let title = 'Welcome';
+			var description = 'Hello, Node.js ^^'; //서버시작 후, description에 본문 생성.
+			fs.readdir('./data', (err, files) => {
+				let lists = FileList(files);
+				let template = templateHTML(title, lists, `<h2>${title}</h2><p>${description}</p>`);
 				response.writeHead(200);
 				response.end(template);
 			});
 		} else {
 			fs.readdir('./data', (err, files) => {
-				let i = 0;
-				let lists = '<ul>';
-				// console.log(files);
-				while (i < 3) {
-					let filelist = files[i];
-					// console.log(filelist);
-					lists = lists + `<li><a href="/?id=${filelist}">${filelist}</a></li>`; //반복 할 때 마다 전에 할당한 <li>를 재할당
-					i = i + 1;
-				}
-				lists = lists + '</ul>';
+				let lists = FileList(files);
 				fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
 					//queryData.id와 일치하는 변수 파일
-					var template = `
-						<!doctype html>
-						<html>
-						<head>
-						<title>WEB1 - ${title}</title>
-						<meta charset="utf-8">
-						</head>
-						<body>
-						<h1><a href="/?id=WEB">WEB</a></h1>
-						${lists}
-						<h2>${title}</h2>
-						<p>${description}
-						</p>
-						</body>
-						</html>
-					`;
+					let title = queryData.id; //http://localhost:3000/?id=ㅁㅁㅁ,	만약 id가 아닌 name이라면 name으로 입력
+					let template = templateHTML(title, lists, `<h2>${title}</h2><p>${description}</p>`);
 					response.writeHead(200);
 					response.end(template);
 				}); //queryData.id로 data폴더 하위 파일을 읽어옴.
---
+++ syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6)); //Math.round: 반올림 함수.
+console.log(Math.round(1.4));
+
+function sum(firstnum, secondnum) {
+	//parameter
+	let total = firstnum + secondnum;
+	return total;
+}
+
+console.log(sum(4, 6)); //argument
